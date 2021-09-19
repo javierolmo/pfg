2d2473bd5a805fc419d189b1befd387b5774c222
JaviOlmoI <44966721+JaviOlmoI@users.noreply.github.com>
2021-09-19 14:18:33 +0200

    Fixes
    -Logs in frontend not displayed because of permissions
    -Github actions flow for genetic composer

--- pfg-frontend/src/app/@core/utils/admin.service.ts
+++ pfg-frontend/src/app/@core/utils/admin.service.ts
@@ -3,18 +3,25 @@ import {HttpClient} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
 import {Log} from '../data/log';
 import {Page} from '../data/page';
+import {NbAuthJWTToken, NbAuthService} from '@nebular/auth';
 
 @Injectable()
 export class AdminService {
 
-    constructor(private httpClient: HttpClient)  {}
+    token: string;
+
+    constructor(private httpClient: HttpClient, protected authService: NbAuthService)  {
+        this.authService.onTokenChange().subscribe((jwttoken: NbAuthJWTToken) => {
+            this.token = jwttoken.getValue();
+        });
+    }
 
     getLogs(index: number, size: number, ascendent?: boolean)  {
         let urlRequest = `${environment.apiUrl}/api/admin/logs/page?`;
         if (index !== undefined) urlRequest += `index=${index}&`;
         if (size !== undefined) urlRequest += `size=${size}&`;
         if (ascendent !== undefined) urlRequest += `ascendent=${ascendent}&`;
-        return this.httpClient.get<Page<Log>>(urlRequest);
+        return this.httpClient.get<Page<Log>>(urlRequest, {headers: {'Authorization': this.token }});
     }
 
 }
